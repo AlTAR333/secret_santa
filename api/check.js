9333e86d88570bf7cfe1098e94ed97d9860f3555
Armand <armand.vieau@epita.fr>
2025-11-24 15:41:17 +0100

    Kitsu enigme

--- api/check.js
+++ api/check.js
@@ -9,13 +9,13 @@ export default function handler(req, res) {
 
   // Hash SHA-256 de chaque mot de passe
   const hashes = {
-    1: '7943242e4f409a4a47f3cb8b184170e0b16b989858191d67a47fdb066ead2dc1', // rododindron
+    1: '$2b$10$yqK1CVZr6zQ6L0H944kaEOoziEtGlUZEM6I72vGUtkLjxJvLC9OtC', // rododindron
     2: '2a5e3b0d3ee31f6c4b9b2f246c8f1b7a6f0e5a0d5c2b5f1e9a6d7f5b3c4a1e8d', // mot de passe étape 2
     3: '3b2f1c0d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2e3f4a5b6c7d8e9f0a1b'  // mot de passe finale
   };
 
   const nextStepUrls = {
-    1: '/api/etape2',
+    1: '/api/rhinoceros',
     2: '/api/finale',
     3: '/api/finale'
   };
