1d189660c30353b58df9648f3ffe16f1b9341a70
Armand <armand.vieau@epita.fr>
2025-11-24 15:55:01 +0100

    Kitsu hash fix

--- api/check.js
+++ api/check.js
@@ -9,7 +9,7 @@ export default function handler(req, res) {
 
   // Hash SHA-256 de chaque mot de passe
   const hashes = {
-    1: '$2b$10$yqK1CVZr6zQ6L0H944kaEOoziEtGlUZEM6I72vGUtkLjxJvLC9OtC', // rododindron
+    1: '52b7acbb78d862906e6d9e96e4c813f2161ab2ec52ffb4058a52c57d20593d67', // Kitsu
     2: '2a5e3b0d3ee31f6c4b9b2f246c8f1b7a6f0e5a0d5c2b5f1e9a6d7f5b3c4a1e8d', // mot de passe étape 2
     3: '3b2f1c0d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2e3f4a5b6c7d8e9f0a1b'  // mot de passe finale
   };
